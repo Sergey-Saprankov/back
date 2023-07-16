4e5214d015fcb183d0cd01b7cb43e65da106a9e4
Sergey_Saprankov <sergey-saprankov@mail.ru>
2023-07-16 16:08:15 +0300

    fix port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 4000
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hi sergik11')
+    res.send('Hi sergik11!!!')
 })
 
 app.listen(port, () => {
